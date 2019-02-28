12528b348f0c845352853e6d1818a0ad2d7c0617
Adam Puchalski <adam.puchalski@capgemini.com>
2019-02-28 14:35:18 +0100

    Problem_001 - functional refactoring

--- Problem_001.js
+++ Problem_001.js
@@ -1,5 +1,21 @@
 "use strict";
 
+function mod(number) {
+	return function next(value) {
+		return value % number;
+	}
+}
+
+// function mod3 (value) {
+// 	return mod(3) (value);
+// }
+
+var mod3 = mod (3);
+
+console.log(mod(2)(11));
+console.log(mod3(8));
+
+
 function isRelevant(number) {
 	return (number % 3 == 0) || (number % 5 == 0);
 }
