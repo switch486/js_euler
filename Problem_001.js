a35795e0dafb4e294c451926d41c193313357479
Adam Puchalski <adam.puchalski@capgemini.com>
2019-02-28 14:07:06 +0100

    Problem_001 procedural solution

--- Problem_001.js
+++ Problem_001.js
@@ -1,10 +1,18 @@
 "use strict";
 
-function PLUS(x, y) { 
-	return function() {
-		return x+y;
-	}
+function isRelevant(number) {
+	return (number % 3 == 0) || (number % 5 == 0);
 }
+
+function countRelevantUpTo(number) {
+	var sum = 0;
+	for (var i = 0; i<number; i++) {
+		sum += isRelevant(i) ? i : 0;
+	}
+	return sum;
+}
+
+console.log(countRelevantUpTo(1000));
 
 
 
